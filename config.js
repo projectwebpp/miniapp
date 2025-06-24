6daef1ba52ffca622ff0771de92ee5b39bdb3048
projectwebpp <124865459+projectwebpp@users.noreply.github.com>
2025-06-24 20:12:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js - ไฟล์การตั้งค่า
-const LIFF_ID = '2007617196-RZDNBQPe';
+const LIFF_ID = '2007617194-X8qdV2Yb';
 const API_URL = 'https://script.google.com/macros/s/AKfycbwS4EwiW66bx8AKD7bt40vcFbZK7xpCod46EeomxBGGfFF_2YARo0xASMSmbPBVnKY0/exec';
 const GOLD_PRICE_API = 'https://script.google.com/macros/s/AKfycbwgvstkxFOR9p6zOV2d8iEGagbpQ6h8C3BhPnDCoB56jvmbAwSG0A9a36r6oRxNkBXQ/exec';
 const GRAMS_PER_BAHT = 15.244;
